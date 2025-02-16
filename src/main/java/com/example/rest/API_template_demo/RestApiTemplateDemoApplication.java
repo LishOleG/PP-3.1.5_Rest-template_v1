4fb37d2b9e0edeb6ead4c5f7ecb24d9b0ad85546
LishOleG <lisholeg16@gmail.com>
2025-02-16 22:18:53 +0500

    внесение изменений nameProject в pom.xml

---
+++ src/main/java/com/example/rest/API_template_demo/RestApiTemplateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest.API_template_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTemplateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiTemplateDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rest/API_template_demo/RestApiTemplateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest.API_template_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiTemplateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
